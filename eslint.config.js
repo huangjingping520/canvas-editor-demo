ed5bb5678dd298631f77a1fc5d3bb3920e8fbf0b
huangjingping <huangjingping@aspirecn.com>
2024-06-13 18:31:11 +0800

    chore: Update eslint.config.js and package.json and add unocss

--- eslint.config.js
+++ eslint.config.js
@@ -2,5 +2,15 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   formatters: true,
-  vue: true,
+  stylistic: {
+    overrides: {
+      'style/comma-dangle': ['error', 'never']
+    }
+  },
+  vue: {
+    overrides: {
+      'vue/comma-dangle': ['error', 'never']
+    }
+  },
+  unocss: true
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  server: {
+    port: 3000
+  },
+  plugins: [
+    vue(),
+    UnoCss()
+  ]
 })
